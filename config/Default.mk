9298b464f367dd483fc0db2fb635c6ecb402b53c
LastRitter <lastritter@gmail.com>
2026-03-13 22:32:23 +0800

    add xen and yocto build config.

--- config/Default.mk
+++ config/Default.mk
@@ -15,7 +15,7 @@ BUILD_TYPE_NUM				?= 3
 BUILD_TYPE_STR				?= test
 BUILD_VARIANT				?= Community
 
-IMAGE_BUILD_LIST			?= U-Boot Linux BusyBox BuildRoot Ubuntu KyberEmu
-IMAGE_DEPEND_LIST			?= 
+IMAGE_BUILD_LIST			?= U-Boot Linux BusyBox BuildRoot Ubuntu KyberEmu Xen Yocto
+IMAGE_DEPEND_LIST			?= Yocto:Xen
 
 
