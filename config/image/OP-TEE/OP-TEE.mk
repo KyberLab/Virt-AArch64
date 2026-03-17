0381e4d8166199072752afdf5144d3e577f05dbc
LastRitter <lastritter@gmail.com>
2026-03-17 18:10:54 +0800

    add op-tee image config.

--- config/image/OP-TEE/OP-TEE.mk
+++ config/image/OP-TEE/OP-TEE.mk
@@ -9,87 +9,47 @@
 IMAGE_BUILD_TYPE			:= Custom
 
 # (Optional) Fetch options
-IMAGE_FETCH_METHOD			:= custom
-IMAGE_FETCH_OPTS			:= aa
-IMAGE_FETCH_URL				:= 
-IMAGE_FETCH_REF				:= 
+IMAGE_FETCH_METHOD			:= repo
+IMAGE_FETCH_OPTS			:= qemu_v8.xml
+IMAGE_FETCH_URL				:= https://github.com/OP-TEE/manifest.git
+IMAGE_FETCH_REF				:= master
 
 # (Optional) Patch options
-IMAGE_PATCH_METHOD			:= custom
-IMAGE_PATCH_OPTS			:= bb
+#IMAGE_PATCH_METHOD			:= 
+IMAGE_PATCH_OPTS			:= 
 
 # (Optional) Config options
 #IMAGE_CONFIG_METHOD		:= 
-IMAGE_CONFIG_OPTS			:= cc
+IMAGE_CONFIG_OPTS			:= 
 
 # (Optional) Build options
 #IMAGE_BUILD_METHOD			:= 
-IMAGE_BUILD_OPTS			:= dd
+IMAGE_BUILD_OPTS			:= 
 
 # (Optional) Install options
-IMAGE_INSTALL_METHOD		:= custom
-IMAGE_INSTALL_OPTS			:= ee
+#IMAGE_INSTALL_METHOD		:= custom
+IMAGE_INSTALL_OPTS			:= 
 IMAGE_INSTALL_LIST			:= 
 
 # (Optional) Package options
 #IMAGE_PACKAGE_METHOD		:= 
-IMAGE_PACKAGE_OPTS			:= ff
+IMAGE_PACKAGE_OPTS			:= 
 IMAGE_PACKAGE_LIST			:= 
 
 # (Optional) Clean options
 #IMAGE_CLEAN_METHOD			:= 
-IMAGE_CLEAN_OPTS			:= gg
+IMAGE_CLEAN_OPTS			:= 
 
 # (Optional) Distclean options
 #IMAGE_DISTCLEAN_METHOD		:= 
-IMAGE_DISTCLEAN_OPTS		:= hh
-
-# (Optional) Distclean options
-#IMAGE_DISTCLEAN_METHOD		:= 
-IMAGE_DISTCLEAN_OPTS		:= hh
+IMAGE_DISTCLEAN_OPTS		:= 
 
 # (Optional) Action options
 IMAGE_ACTION_METHOD			:= custom
-IMAGE_ACTION_OPTS			:= gg
+IMAGE_ACTION_OPTS			:= 
 
 
 # (Optional) Extra step
-
-# image_custom_fetch
-# $(1) fetch options
-# $(2) config path
-# $(3) build path
-# $(4) install path
-# $(5) fetch url
-# $(6) fetch ref
-define image_custom_fetch
-	$(IQ)$(call xprint_title,	"Image $(IMAGE_BUILD_GOAL) Fetch",$(BG_YELLOW))
-	$(IQ)$(call xprint_value,	"Fetch Options",	$(1),$($(BG_PURPLE)))
-	$(IQ)$(call xprint_value,	"Config Path",		$(2),$($(BG_PURPLE)))
-	$(IQ)$(call xprint_value,	"Build Path",		$(3),$($(BG_PURPLE)))
-	$(IQ)$(call xprint_value,	"Install Path",		$(4),$($(BG_PURPLE)))
-	$(IQ)$(call xprint_value,	"Fetch URL",		$(5),$($(BG_PURPLE)))
-	$(IQ)$(call xprint_value,	"Fetch REF",		$(6),$($(BG_PURPLE)))
-	$(IQ)$(call xprint_line,$(BG_YELLOW))
-	$(IQ)mkdir -pv $(3)
-endef
-
-
-# image_custom_patch
-# $(1) patch options
-# $(2) config path
-# $(3) build path
-# $(4) install path
-define image_custom_patch
-	$(IQ)$(call xprint_title,	"Image $(IMAGE_BUILD_GOAL) Patch",$(BG_YELLOW))
-	$(IQ)$(call xprint_value,	"Patch Options",	$(1),$($(BG_PURPLE)))
-	$(IQ)$(call xprint_value,	"Config Path",		$(2),$($(BG_PURPLE)))
-	$(IQ)$(call xprint_value,	"Build Path",		$(3),$($(BG_PURPLE)))
-	$(IQ)$(call xprint_value,	"Install Path",		$(4),$($(BG_PURPLE)))
-	$(IQ)$(call xprint_value,	"Patch Files",		$(wildcard $(2)/Patches/*.patch),$($(BG_PURPLE)))
-	$(IQ)$(call xprint_line,$(BG_YELLOW))
-endef
-
 
 # image_custom_config
 # $(1) config options
@@ -118,6 +78,7 @@ define image_custom_build
 	$(IQ)$(call xprint_value,	"Build Path",		$(3),$($(BG_PURPLE)))
 	$(IQ)$(call xprint_value,	"Install Path",		$(4),$($(BG_PURPLE)))
 	$(IQ)$(call xprint_line,$(BG_YELLOW))
+	$(IQ)cd $(3)/build && make -j$$(nproc) toolchains && make -j$$(nproc) FORCE_UNSAFE_CONFIGURE=1 GDBSERVER=y
 endef
 
 
@@ -134,23 +95,6 @@ define image_custom_install
 	$(IQ)$(call xprint_value,	"Build Path",		$(3),$($(BG_PURPLE)))
 	$(IQ)$(call xprint_value,	"Install Path",		$(4),$($(BG_PURPLE)))
 	$(IQ)$(call xprint_value,	"Install List",		$(5),$($(BG_PURPLE)))
-	$(IQ)$(call xprint_line,$(BG_YELLOW))
-endef
-
-
-# image_custom_package
-# $(1) package options
-# $(2) config path
-# $(3) build path
-# $(4) install path
-# $(5) package list
-define image_custom_package
-	$(IQ)$(call xprint_title,	"Image $(IMAGE_BUILD_GOAL) Package",$(BG_YELLOW))
-	$(IQ)$(call xprint_value,	"Package Options",	$(1),$($(BG_PURPLE)))
-	$(IQ)$(call xprint_value,	"Config Path",		$(2),$($(BG_PURPLE)))
-	$(IQ)$(call xprint_value,	"Build Path",		$(3),$($(BG_PURPLE)))
-	$(IQ)$(call xprint_value,	"Install Path",		$(4),$($(BG_PURPLE)))
-	$(IQ)$(call xprint_value,	"Package List",		$(5),$($(BG_PURPLE)))
 	$(IQ)$(call xprint_line,$(BG_YELLOW))
 endef
 
@@ -197,5 +141,7 @@ define image_custom_action
 	$(IQ)$(call xprint_value,	"Build Path",		$(3),$($(BG_PURPLE)))
 	$(IQ)$(call xprint_value,	"Install Path",		$(4),$($(BG_PURPLE)))
 	$(IQ)$(call xprint_line,$(BG_YELLOW))
+	$(IQ)cd $(3)/build && make run QEMU_USERNET_ENABLE=y FORCE_UNSAFE_CONFIGURE=1
 endef
+
 
